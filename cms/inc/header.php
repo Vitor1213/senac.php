5aba8827a71d2caa617b777e657f0f58a4630714
Vitor Hugo <vitor1213.dev@gmail.com>
2019-06-25 19:34:17 -0300

    header atualizada

--- cms/inc/header.php
+++ cms/inc/header.php
@@ -8,7 +8,7 @@
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Jekyll v3.8.5">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
-    <title>Meu site</title>
+    <title>Tudo sobre jogos</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/carousel/">
 
@@ -26,11 +26,6 @@
         user-select: none;
       }
 	  
-	  div.container marketing i {
-		  margin: 36rem;
-		  padding: 40rem;
-	  }
-
       @media (min-width: 768px) {
         .bd-placeholder-img-lg {
           font-size: 3.5rem;
@@ -43,24 +38,32 @@
   <body>
     <header>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <a class="navbar-brand" href="#">Carousel</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <i class="fas fa-gamepad" style="color:white;"></i>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
           <a class="nav-link" href="index.php">Home</a>
         </li>
+		<li class="nav-item active">
+          <a class="nav-link" href="index.php">Fotos</a>
+        </li>
+		<li class="nav-item active">
+          <a class="nav-link" href="index.php">Contatos</a>
+        </li>
+		<li class="nav-item active">
+          <a class="nav-link" href="index.php">Sobre</a>
+        </li>
 		
 		
       </ul>
       <form class="form-inline mt-2 mt-md-0">
-        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <input class="form-control mr-sm-2" type="text" placeholder="Localizar" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
       </form>
     </div>
   </nav>
 </header>
 
+<main role="main">
 <main role="main">
