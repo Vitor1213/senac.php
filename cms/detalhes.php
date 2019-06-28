00c54f9972c8675fc0abb68d50b72d184ef0eb34
Vitor Hugo <vitor1213.dev@gmail.com>
2019-06-27 21:50:27 -0300

    Página gameplay criada, detalhes atualizada

--- cms/detalhes.php
+++ cms/detalhes.php
@@ -17,7 +17,7 @@
       <div class="col-md-4">
         <h2>Gameplays</h2>
         <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn btn-secondary" href="#" role="button">Detalhes &raquo;</a></p>
+        <p><a class="btn btn-secondary" href="detalhes1.php" role="button">Detalhes &raquo;</a></p>
       </div>
       <div class="col-md-4">
         <h2>Novos jogos</h2>
---
+++ cms/detalhes1.php
@@ -0,0 +1,79 @@
+<?php 
+	require_once("inc/header.php"); 
+?>
+
+  <div class="jumbotron">
+    <div class="container">
+      <h1 class="display-4">Gameplays</h1>
+      <p>Acompanhe os melhores pro-players da atualidade e assista os últimos confrontos entre os players no mundial de LoL (League of Legends World Championship).</p>
+      <!--<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>-->
+    </div>
+  </div>
+<main role="main" class="container">
+  <div class="my-3 p-3 bg-white rounded shadow-sm">
+    <h6 class="border-bottom border-gray pb-2 mb-0">Atualizações recentes</h6>
+    <div class="media text-muted pt-3">
+      <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+      <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+        <strong class="d-block text-gray-dark">@Draven - brTT</strong>
+       <a href="https://youtu.be/0Yv6Xld-or0">Clique aqui para vê-lo em ação!</a>
+      </p>
+    </div>
+    <div class="media text-muted pt-3">
+      <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#e83e8c"/><text x="50%" y="50%" fill="#e83e8c" dy=".3em">32x32</text></svg>
+      <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+        <strong class="d-block text-gray-dark">@Katarina - YoDa</strong>
+        <a href="https://youtu.be/0Yv6Xld-or0">Clique aqui para vê-lo em ação!</a>
+      </p>
+    </div>
+    <div class="media text-muted pt-3">
+      <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#6f42c1"/><text x="50%" y="50%" fill="#6f42c1" dy=".3em">32x32</text></svg>
+      <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+        <strong class="d-block text-gray-dark">@Vladimir - Rakin</strong>
+       <a href="https://youtu.be/enQ5GkpGf8o">Clique aqui para vê-lo em ação!</a>
+      </p>
+    </div>
+    <small class="d-block text-right mt-3">
+      <a href="https://www.techtudo.com.br/listas/2019/04/league-of-legends-confira-os-campeoes-mais-famosos-de-pro-players-esports.ghtml">Todas as atualizações</a>
+    </small>
+  </div>
+
+  <div class="my-3 p-3 bg-white rounded shadow-sm">
+    <h6 class="border-bottom border-gray pb-2 mb-0">Sugestões</h6>
+    <div class="media text-muted pt-3">
+      <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+      <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+        <div class="d-flex justify-content-between align-items-center w-100">
+          <strong class="text-gray-dark">Aleksandr Kostyliev</strong>
+          <a href="#">Seguir</a>
+        </div>
+        <span class="d-block">@S1mple</span>
+      </div>
+    </div>
+    <div class="media text-muted pt-3">
+      <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+      <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+        <div class="d-flex justify-content-between align-items-center w-100">
+          <strong class="text-gray-dark">Mathieu Herbaut</strong>
+          <a href="#">Seguir</a>
+        </div>
+        <span class="d-block">@ZywOo</span>
+      </div>
+    </div>
+    <div class="media text-muted pt-3">
+      <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+      <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+        <div class="d-flex justify-content-between align-items-center w-100">
+          <strong class="text-gray-dark">Ali Djami</strong>
+          <a href="#">Seguir</a>
+        </div>
+        <span class="d-block">@Jame</span>
+      </div>
+    </div>
+    <small class="d-block text-right mt-3">
+      <a href="https://www.esports.net/top-5-csgo-players-in-2019/">Todas as sugestões</a>
+    </small>
+  </div>
+</main>
+  
+ <?php require_once("inc/footer.php"); ?>
